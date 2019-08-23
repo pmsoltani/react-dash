8f03eedd71d9aa044adc7d7959801f44cee901ac
Pooria Soltani <pooria.ms@gmail.com>
2019-08-23 21:21:25 +0430

    installed ant.design, amcharts, and other tools along with some modifications to the CRA boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layout } from "antd";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PageFooter from "./PageFooter";
+// import Navbar from "./Navbar";
+import PageHeader from "./PageHeader";
+import Dashboard from "./Dashboard";
+
+const { Footer, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <div id="page-container">
+        <PageHeader />
+
+        <Content id="page-content">
+          <Dashboard />
+        </Content>
+
+        <Layout>
+          <Footer id="page-footer">
+            <PageFooter />
+          </Footer>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
