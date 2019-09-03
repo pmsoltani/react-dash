7045b28556bd9fdb31b9dfe0aa5b63c35f378afe
Pooria Soltani <pooria.ms@gmail.com>
2019-09-03 15:58:30 +0430

    duplicated 'ScoreCards' component to work on a new design

--- src/ScoreCards.js
+++ src/ScoreCards.js
@@ -1,46 +1,75 @@
 import React, { Component } from "react";
-import { Row, Col, Card, Typography } from "antd";
+import { Row, Col, Card, Tabs, Typography } from "antd";
 import Elevation from "./Elevation";
 import "./ScoreCards.css";
+import AmSparkBar from "./AmSparkBar";
+import AmChordChart from "./AmChordChart";
+import AmSparkLine from "./AmSparkLine";
+import AmMixedChart from "./AmMixedChart";
+import AmWordCloud from "./AmWordCloud";
 
 const { Title, Text } = Typography;
+const { TabPane } = Tabs;
+const styles = {
+  width: "100%",
+  height: "300px"
+}
 
 class ScoreCards extends Component {
   render() {
     return (
       <div className="cards-container">
-        <Row type="flex" gutter={32} justify="center" className="cards-row">
-          <Col xs={20} lg={5} className="card-container">
-            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
-              <Card width="100%" className="card papers">
-                <Title level={2}>16</Title>
-                <Text>papers this year</Text>
-              </Card>
-            </Elevation>
+        <Row type="flex" justify="center" className="cards-row">
+          <Col xs={24} lg={16}>
+            <Tabs defaultActiveKey="1" size="small" animated={{ tabPane: false }}>
+              <TabPane tab="Tab 1" key="1">
+                <AmMixedChart styles={styles} />
+              </TabPane>
+              <TabPane tab="Tab 2" key="2">
+                <AmChordChart styles={styles} />
+              </TabPane>
+              <TabPane tab="Tab 3" key="3">
+                <AmWordCloud styles={styles} />
+              </TabPane>
+            </Tabs>
           </Col>
-          <Col xs={20} lg={5} className="card-container">
-            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
-              <Card width="100%" className="card citations">
-                <Title level={2}>386</Title>
-                <Text>papers this year</Text>
-              </Card>
-            </Elevation>
+          <Col
+            xs={24}
+            lg={2}
+            style={{ marginTop:"40px", borderRadius: 0, height: "200px" }}
+            className="card papers"
+          >
+            <Title level={2}>16</Title>
+            <Text>papers this year</Text>
+            <AmSparkBar />
           </Col>
-          <Col xs={20} lg={5} className="card-container">
-            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
-              <Card width="100%" className="card national">
-                <Title level={2}>5</Title>
-                <Text>national collaborators</Text>
-              </Card>
-            </Elevation>
+          <Col
+            xs={24}
+            lg={2}
+            style={{ marginTop:"40px", borderRadius: 0, height: "200px" }}
+            className="card citations"
+          >
+            <Title level={2}>386</Title>
+            <Text>papers this year</Text>
+            <AmSparkLine />
           </Col>
-          <Col xs={20} lg={5} className="card-container">
-            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
-              <Card width="100%" className="card international">
-                <Title level={2}>3</Title>
-                <Text>international collaborators</Text>
-              </Card>
-            </Elevation>
+          <Col
+            xs={24}
+            lg={2}
+            style={{ marginTop:"40px", borderRadius: 0, height: "200px" }}
+            className="card national"
+          >
+            <Title level={2}>5</Title>
+            <Text>national collaborators</Text>
+          </Col>
+          <Col
+            xs={24}
+            lg={2}
+            style={{ marginTop:"40px", borderRadius: 0, height: "200px" }}
+            className="card international"
+          >
+            <Title level={2}>3</Title>
+            <Text>international collaborators</Text>
           </Col>
         </Row>
       </div>
---
+++ src/ScoreCards2.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Row, Col, Card, Typography } from "antd";
+import Elevation from "./Elevation";
+import "./ScoreCards.css";
+
+const { Title, Text } = Typography;
+
+class ScoreCards extends Component {
+  render() {
+    return (
+      <div className="cards-container">
+        <Row type="flex" gutter={32} justify="center" className="cards-row">
+          <Col xs={20} lg={5} className="card-container">
+            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
+              <Card width="100%" className="card papers">
+                <Title level={2}>16</Title>
+                <Text>papers this year</Text>
+                <Title level={2}>16</Title>
+                <Title level={2}>16</Title>
+                <Title level={2}>16</Title>
+              </Card>
+            </Elevation>
+          </Col>
+          <Col xs={20} lg={5} className="card-container">
+            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
+              <Card width="100%" className="card citations">
+                <Title level={2}>386</Title>
+                <Text>papers this year</Text>
+              </Card>
+            </Elevation>
+          </Col>
+          <Col xs={20} lg={5} className="card-container">
+            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
+              <Card width="100%" className="card national">
+                <Title level={2}>5</Title>
+                <Text>national collaborators</Text>
+              </Card>
+            </Elevation>
+          </Col>
+          <Col xs={20} lg={5} className="card-container">
+            <Elevation depth={1} styles={{ borderRadius: "10px" }}>
+              <Card width="100%" className="card international">
+                <Title level={2}>3</Title>
+                <Text>international collaborators</Text>
+              </Card>
+            </Elevation>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ScoreCards;
