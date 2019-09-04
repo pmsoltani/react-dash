0b43e520449a8a0711de52f714c4c386bd62ee82
Pooria Soltani <pooria.ms@gmail.com>
2019-09-04 07:43:02 +0430

    removed the elevation and background from the component

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Typography, List, Avatar } from "antd";
+import { Row, Col, Card, Typography, List, Avatar } from "antd";
 import Elevation from "./Elevation";
 import "./UserInfo.css";
 
@@ -28,7 +28,7 @@ const contactInfo = [
     icon: linkedinLogo,
     text: "linkedin.com/in/pmsoltani"
   },
-  { type: "phone", address: "", icon: phoneLogo, text: "+98 939 137 0620" }
+  // { type: "phone", address: "", icon: phoneLogo, text: "+98 939 137 0620" }
 ];
 
 class UserInfo extends Component {
@@ -37,54 +37,51 @@ class UserInfo extends Component {
       <div className="user-info-container">
         <Row type="flex" justify="center">
           <Col xs={24} className="user-info">
-            <Elevation
-              depth={1}
-              styles={{
-                borderRadius: "10px",
-                padding: "24px",
-                backgroundColor: "#fff"
-              }}
-            >
-              <Row type="flex" justify="center" className="general-info">
-                <Col xs={24} lg={4}>
-                  <Avatar size={120} icon="user" src={pooria} shape="square" />
-                </Col>
-                <Col xs={24} lg={8}>
-                  <Title level={2}>Pooria Soltani</Title>
-                  Associate Professor
-                  <br />
-                  <Text>
-                    <a href="/">Department of Chemical Engineering</a>
-                  </Text>
-                  <br />
-                  <Text>
-                    <a href="/">Sharif University of Technology</a>
-                  </Text>
-                </Col>
-                <Col xs={24} lg={4} />
-                <Col xs={24} lg={8} style={{ flexGrow: 2, textAlign: "right" }}>
-                  <List
-                    dataSource={contactInfo}
-                    renderItem={item => (
-                      <List.Item
-                        style={{
-                          borderBottom: 0,
-                          padding: 0,
-                          paddingBottom: "6px"
-                        }}
-                      >
-                        <a href={item.address}>
-                          <List.Item.Meta
-                            avatar={<Avatar src={item.icon} />}
-                            title={item.text}
-                          />
-                        </a>
-                      </List.Item>
-                    )}
-                  />
-                </Col>
-              </Row>
-            </Elevation>
+            <Row type="flex" justify="center" className="general-info">
+              <Col xs={24} lg={4}>
+                <Avatar
+                  size={120}
+                  icon="user"
+                  src={pooria}
+                  style={{ border: "3px solid #8739e5" }}
+                  // shape="square"
+                />
+              </Col>
+              <Col xs={24} lg={8}>
+                <Title level={2}>Pooria Soltani</Title>
+                Associate Professor
+                <br />
+                <Text>
+                  <a href="/">Department of Chemical Engineering</a>
+                </Text>
+                <br />
+                <Text>
+                  <a href="/">Sharif University of Technology</a>
+                </Text>
+              </Col>
+              <Col xs={24} lg={4} />
+              <Col xs={24} lg={8} style={{ flexGrow: 2, textAlign: "right" }}>
+                <List
+                  dataSource={contactInfo}
+                  renderItem={item => (
+                    <List.Item
+                      style={{
+                        borderBottom: 0,
+                        padding: 0,
+                        paddingBottom: "6px"
+                      }}
+                    >
+                      <a href={item.address}>
+                        <List.Item.Meta
+                          avatar={<Avatar src={item.icon} />}
+                          title={item.text}
+                        />
+                      </a>
+                    </List.Item>
+                  )}
+                />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
