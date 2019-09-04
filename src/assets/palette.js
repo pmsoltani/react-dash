f619584bbc1e0c60de6a21f18ea0e25a50136dbb
Pooria Soltani <pooria.ms@gmail.com>
2019-09-04 07:41:55 +0430

    added a new palette consisting of 8 colors, each having light and dark shades

---
+++ src/assets/palette.js
@@ -0,0 +1,27 @@
+const palette = [
+  "#ffdf40",
+  "#ff8359",
+
+  "#Aff57a",
+  "#54d169",
+
+  "#46eeaa",
+  "#2cbfc7",
+
+  "#15eded",
+  "#029cf5",
+
+  "#59c2ff",
+  "#1270e3",
+
+  "#5496ff",
+  "#8739e5",
+
+  "#ff63de",
+  "#b122e5",
+
+  "#ff7c6e",
+  "#f5317f"
+];
+
+export default palette;
