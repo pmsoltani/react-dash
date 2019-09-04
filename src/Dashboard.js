dcf93a38191a83f5085255ac46533f2fda2f320e
Pooria Soltani <pooria.ms@gmail.com>
2019-09-04 16:01:27 +0430

    Added a horizontal divider after the 'UserInfo' component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,12 +4,14 @@ import ScoreCards from "./ScoreCards";
 import ChartTable from "./ChartTable";
 
 import "./Dashboard.css";
+import { Divider } from "antd";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard-container">
         <UserInfo />
+        <Divider style={{height: "2px"}} />
         <ScoreCards />
         <ChartTable />
       </div>
