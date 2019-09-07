42441f2d5aa461c6a06f9230808750d7615dd130
Pooria Soltani <pooria.ms@gmail.com>
2019-09-07 14:50:46 +0430

    Modified 'UserInfo' component to show contact info in a modal and get the information from parent ('Dashboard')

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
+import { Divider } from "antd";
 import UserInfo from "./UserInfo";
 import ScoreCards from "./ScoreCards";
-import ChartTable from "./ChartTable";
-
+// import ChartTable from "./ChartTable";
 import "./Dashboard.css";
-import { Divider } from "antd";
+import Papers from "./Papers";
+
+const info = {
+  first: "Pooria",
+  last: "Soltani",
+  rank: "Associate Professor",
+  department: "Department of Chemical Engineering",
+  institution: "Sharif University of Technology",
+  contact: [
+    { type: "email", address: "/", text: "pooria.ms@gmail.com" },
+    { type: "website", address: "/", text: "sharif.edu/~pmsoltani" },
+    { type: "scholar", address: "/", text: "www.google.com" },
+    { type: "linkedin", address: "/", text: "linkedin.com/in/pmsoltani" },
+    { type: "phone", address: "", text: "+98 939 137 0620" }
+  ]
+};
+
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard-container">
-        <UserInfo />
-        <Divider style={{height: "2px"}} />
+        <UserInfo
+          first={info.first}
+          last={info.last}
+          rank={info.rank}
+          department={info.department}
+          institution={info.institution}
+          contactInfo={info.contact}
+        />
+        <Divider style={{ height: "2px" }} />
         <ScoreCards />
-        <ChartTable />
+        <Papers />
       </div>
     );
   }
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,37 +1,56 @@
 import React, { Component } from "react";
-import { Typography, List, Avatar } from "antd";
+import { Typography, List, Avatar, Modal, Button, Icon, Popover } from "antd";
 import "./UserInfo.css";
 import { Container, Row, Col } from "react-grid-system";
 
 // import profile from "./assets/profile.svg";
 import linkedinLogo from "./assets/linkedin.svg";
 import emailLogo from "./assets/email.svg";
-// import phoneLogo from "./assets/phone.svg";
+import phoneLogo from "./assets/phone.svg";
 import googleLogo from "./assets/google.svg";
 import websiteLogo from "./assets/website.svg";
 import pooria from "./assets/pooria.jpg"
 
 const { Title, Text } = Typography;
 
-const contactInfo = [
-  { type: "email", address: "/", icon: emailLogo, text: "pooria.ms@gmail.com" },
-  {
-    type: "website",
-    address: "/",
-    icon: websiteLogo,
-    text: "sharif.edu/~pmsoltani"
-  },
-  { type: "scholar", address: "/", icon: googleLogo, text: "www.google.com" },
-  {
-    type: "linkedin",
-    address: "/",
-    icon: linkedinLogo,
-    text: "linkedin.com/in/pmsoltani"
-  },
-  // { type: "phone", address: "", icon: phoneLogo, text: "+98 939 137 0620" }
-];
+const contactIcons = {
+  email: emailLogo ,
+  website: websiteLogo ,
+  scholar: googleLogo ,
+  linkedin: linkedinLogo ,
+  phone: phoneLogo
+};
 
 class UserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContactModal: false,
+      contactInfo: this.props.contactInfo.map(
+        item => ({...item, icon: contactIcons[item.type]})
+      )
+    };
+  }
+
+  showModal = () => {
+    this.setState({
+      showContactModal: true
+    });
+  };
+
+  handleCancel = e => {
+    this.setState({
+      showContactModal: false
+    });
+  };
+
+  popMessage = (
+    <div>
+      <Icon type="tool" />
+      <span style={{ marginLeft: "8px" }}>Under Construction!</span>
+    </div>
+  );
+
   render() {
     return (
       <Container fluid className="user-info-container">
@@ -45,37 +64,70 @@ class UserInfo extends Component {
             />
           </Col>
           <Col>
-            <Title level={2}>Pooria Soltani</Title>
-            Associate Professor
+            <Title level={2}>
+              <span className="first-name">{this.props.first}</span>
+              <span> </span>
+              <span
+                className="last-name"
+                style={{ textTransform: "uppercase" }}
+              >
+                {this.props.last}
+              </span>
+            </Title>
+            {this.props.rank}
             <br />
             <Text>
-              <a href="/">Department of Chemical Engineering</a>
+              <a href="/">{this.props.department}</a>
             </Text>
             <br />
             <Text>
-              <a href="/">Sharif University of Technology</a>
+              <a href="/">{this.props.institution}</a>
             </Text>
           </Col>
           <Col xs={24} md="content">
-            <List
-              dataSource={contactInfo}
-              renderItem={item => (
-                <List.Item
-                  style={{
-                    borderBottom: 0,
-                    padding: 0,
-                    paddingBottom: "6px"
-                  }}
-                >
-                  <a href={item.address}>
-                    <List.Item.Meta
-                      avatar={<Avatar src={item.icon} />}
-                      title={item.text}
-                    />
-                  </a>
-                </List.Item>
-              )}
-            />
+            <Button type="primary" icon="info-circle" onClick={this.showModal}>
+              Contact Info
+            </Button>
+            <br />
+            <br />
+            <Popover
+              placement="left"
+              // title="hi"
+              content={this.popMessage}
+              trigger="click"
+            >
+              <Button type="default" icon="edit">
+                Suggest Edit
+              </Button>
+            </Popover>
+
+            <Modal
+              title="Contact"
+              visible={this.state.showContactModal}
+              onCancel={this.handleCancel}
+              closable
+              footer={null}
+            >
+              <List
+                dataSource={this.state.contactInfo}
+                renderItem={item => (
+                  <List.Item
+                    style={{
+                      borderBottom: 0,
+                      padding: 0,
+                      paddingBottom: "6px"
+                    }}
+                  >
+                    <a href={item.address}>
+                      <List.Item.Meta
+                        avatar={<Avatar src={item.icon} />}
+                        title={item.text}
+                      />
+                    </a>
+                  </List.Item>
+                )}
+              />
+            </Modal>
           </Col>
         </Row>
       </Container>
