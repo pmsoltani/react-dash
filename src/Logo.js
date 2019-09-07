f7862be16ef37855baa62efdb104ccf7a7ef2120
Pooria Soltani <pooria.ms@gmail.com>
2019-09-08 00:50:05 +0430

    Re-created the 'Logo' component using the 'Pacifico' font.

--- src/Logo.js
+++ src/Logo.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import logo from "./assets/wieldyLogo.png";
+import "./Logo.css";
 
 class Logo extends Component {
   render() {
     return (
       <div
         className="logo-container"
-        style={{ width: this.props.width, height: this.props.height }}
+        style={{ fontFamily: "Pacifico", ...this.props.style }}
       >
-        <img src={logo} alt="app logo" width="100%" height="100%" />
-        {/* <div>elsametric</div> */}
+        elsametric
       </div>
     );
   }
