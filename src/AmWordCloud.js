5a15edf0c7e7077c2c51093b5030b67f6c5ce437
Pooria Soltani <pooria.ms@gmail.com>
2019-09-07 14:27:12 +0430

    Disabled the 'material' theme.

--- src/AmWordCloud.js
+++ src/AmWordCloud.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
+// import * as am4charts from "@amcharts/amcharts4/charts";
 import * as am4plugins_wordCloud from "@amcharts/amcharts4/plugins/wordCloud";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import am4themes_material from "@amcharts/amcharts4/themes/material";
+// import am4themes_material from "@amcharts/amcharts4/themes/material";
 // import "./AmWordCloud.css";
 
-// am4core.useTheme(am4themes_animated);
+am4core.useTheme(am4themes_animated);
 // am4core.useTheme(am4themes_material);
 
 const data = [
